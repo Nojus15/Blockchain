44b7a89d63f8e5d790c6371a9be2a6df1392b3cb
Nojus <53999653+Nojus15@users.noreply.github.com>
2022-10-17 23:29:12 +0300

    Added file handler

---
+++ File/File.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::stringstream;
+
+class File
+{
+private:
+    void openFile(std::ifstream &open_f, string file_name);
+
+public:
+    void writeFile(string file_name, stringstream &ss);
+    stringstream readFile(string file_name);
+};
